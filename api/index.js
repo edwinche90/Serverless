54f0d33f303b6e4f45365d7ffce209dc183ae58b
edwinche90 <78755895+edwinche90@users.noreply.github.com>
2022-02-02 18:01:45 -0600

    2 de febero

--- api/index.js
+++ api/index.js
@@ -4,11 +4,6 @@ const app = express();
 app.get('*', (req, res) => {
     console.log('Hola mundo! mi primera aplicacion serverless');
     res.send({mensaje: 'Chanchito Feliz'})
-})
-
-export default function handler(request, response) {
-    const { name } = request.query;
-    response.status(200).send(`Hello ${name}!`);
-  }
+});
 
 module.exports = app
