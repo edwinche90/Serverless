9ac30ec4272f8f425fd0fea9560158b2e3a033ee
edwinche90 <78755895+edwinche90@users.noreply.github.com>
2022-02-09 17:39:36 -0600

    8 de febrero

--- api/index.js
+++ api/index.js
@@ -1,9 +1,26 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express();
+const MONGODB_URI = `mongodb+srv://almuerzimaster:41065029@almuerzi-db.wcjuj.mongodb.net/Almuerzi-db?retryWrites=true&w=majority`
 
-app.get('*', (req, res) => {
+
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,
+})
+.then((db) => console.log("Mongodb esta conectado en:", db.connection.host))
+.catch((err) =>console.error(err));
+
+
+const Users = mongoose.model('User', new mongoose.schema({name:String}))
+
+Users.create({name:'chanchito triste'})
+app.get('/index.jsn', (req, res)=>{
+    Users.find()
+    .then(x => res.send(x))
+})
+/*app.get('*', (req, res) => {
     console.log('Hola mundo! mi primera aplicacion serverless');
     res.send({mensaje: 'Chanchito Feliz'})
-});
+});*/
 
 module.exports = app
