2da4aaaf62318378b1800a9f83a11035e665348f
edwinche90 <78755895+edwinche90@users.noreply.github.com>
2022-02-09 21:58:39 -0600

    8 de febrero a las 10:00

--- api/index.js
+++ api/index.js
@@ -1,25 +1,29 @@
+const express = require('express');
 const mongoose = require('mongoose')
-const express = require('express')
-const app = express();
-const MONGODB_URI = `mongodb+srv://almuerzimaster:41065029@almuerzi-db.wcjuj.mongodb.net/Almuerzi-db?retryWrites=true&w=majority`
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
+const app = express()
+require('dotenv').config();
+
+app.use(bodyParser.json())
+app.use(cors())
 
 
+//const MONGODB_URI = `mongodb+srv://almuerzimaster:41065029@almuerzi-db.wcjuj.mongodb.net/almuerzi?retryWrites=true&w=majority`
 
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,
 })
 .then((db) => console.log("Mongodb esta conectado en:", db.connection.host))
 .catch((err) =>console.error(err));
 
 
-const users = mongoose.model('User', new mongoose.schema({name:String}))
-users.create({name:'chanchito triste'})
-app.get('*', (req, res)=>{
-    users.find()
-    .then(x => res.send(x))
-})
-/*app.get('*', (req, res) => {
-    console.log('Hola mundo! mi primera aplicacion serverless');
-    res.send({mensaje: 'Chanchito Feliz'})
-});*/
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
 
 module.exports = app
+
+// app.get('*', (req, res) =>  {
+//     res.send('Hola Mundo')
+// });
